f55817452895b7570025eb3497d5d0100a1786b5
Sudheer N <nsudheer515@gmail.com>
2025-06-16 00:03:11 +0530

    Student app changes

---
+++ src/main/java/com/student/contoller/StudentDetailsController.java
@@ -0,0 +1,14 @@
+package com.student.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentDetailsController {
+	
+	@GetMapping("/greet")
+	public String getGreetStudent() {
+		return "Hi student, welcome to 2025 academic year !";
+	}
+
+}
