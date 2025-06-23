139aad637292960d513b88ba2b669968f12e4140
Sudheer N <nsudheer515@gmail.com>
2025-06-23 14:59:52 +0530

    Added Bean life cycle methods

--- src/main/java/com/student/service/SwimmingCoach.java
+++ src/main/java/com/student/service/SwimmingCoach.java
@@ -2,12 +2,27 @@ package com.student.service;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Service
 public class SwimmingCoach implements CoachService{
+	
+	// define our init method
+	@PostConstruct
+	public void doMyStartUpStuff() {
+		System.out.println("In doMyStartUpStuff : "+getClass().getSimpleName());
+	}
 
 	@Override
 	public String giveInstructions() {
 		return "Practice Swimming daily upto 3 hrs !";
+	}
+	
+	// define our destroy method
+	@PreDestroy
+	public void doMyCleanUpStuff() {
+		System.out.println("In doMyCleanUpStuff : "+getClass().getSimpleName());
 	}
 
 }
